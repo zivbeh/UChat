a967e243b41727754a4d181b7f2e7518a71feaa1
Ziv Behar <zivbeh@gmail.com>
2021-03-20 21:13:24 +0200

    added a button to send messages

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -8,6 +8,7 @@
 
     const $panel = $('.cons');
     const $myMessageBox = $('.my-message-box');
+    const $myMessageButton = $('.fa-paper-plane');
 
     const $roomList = $('.room-list');
 
@@ -160,43 +161,25 @@
         server.deleteRoom(Id);
     });
 
+    function WhatToDoWhenSendingAMessage(){
+        const messageText = $myMessageBox.val();
+        if (messageText == '' || messageText.startsWith(" ") == true){
+            return;
+        }
+        $myMessageBox.val('');
+        
+        server.sendMessage(messageText, $('.room.active').attr('id'));
+    }
+
     $myMessageBox.on('keydown', function (ev) {
         if(ev.keyCode === 13){
-            const messageText = $myMessageBox.val();
-            $myMessageBox.val('');
-            
-            server.sendMessage(messageText, $('.room.active').attr('id'));
+            WhatToDoWhenSendingAMessage();
         }else{
             return;
         }
     });
+
+    $myMessageButton.click(function() {
+        WhatToDoWhenSendingAMessage();
+    });
 }());
-
-
-/* <script>
-//         <% activeRoom.forEach(msg => { %>
-//     <% if(user.dataValues.id === msg.id){ %>
-//         <div class="poc">
-//             <div class="_2hqOq message-in" tabindex="-1">
-//                 <div class="_2et95 my">
-//                     <span data-testid="tail-in" data-icon="tail-in" class="_2-dPL"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 13" width="8" height="13"><path opacity=".13" fill="#0000000" d="M1.533 3.568L8 12.193V1H2.812C1.042 1 .474 2.156 1.533 3.568z"></path><path fill="currentColor" d="M1.533 2.568L8 11.193V0H2.812C1.042 0 .474 1.156 1.533 2.568z"></path></svg></span>
-//                     <span dir="auto" class="_3Whw5">
-//                         <span class="messagon"><%= msg.message %></span>
-//                     </span>
-//                 </div>
-//             </div>
-//         </div>
-//     <% } else { %>
-//         <div class="poc">
-//             <div class="_2hqOq message-out" tabindex="-1">
-//                 <div class="_2et95 _2et95-No-ziv">
-//                     <span data-testid="tail-out" data-icon="tail-out" class="_2-dPL"><svg xmlns="http://www.w3.org/2000/svg" class="friend" viewBox="0 0 8 13" width="8" height="13"><path opacity=".13" d="M5.188 1H0v11.193l6.467-8.625C7.526 2.156 6.958 1 5.188 1z"></path><path fill="currentColor" d="M5.188 0H0v11.193l6.467-8.625C7.526 1.156 6.958 0 5.188 0z"></path></svg></span>
-//                     <span dir="auto" class="_3Whw5">
-//                         <div class="parent"><b class="hi"><%= msg.UserId %></b></div><span class="messagon"><%= msg.message %></span>
-//                     </span>
-//                 </div>
-//             </div>
-//         </div>
-//     <% } %>
-// <% }) %>
-</script> */
